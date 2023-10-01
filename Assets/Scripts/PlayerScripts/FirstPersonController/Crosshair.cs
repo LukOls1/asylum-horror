3085ebd38079a5ea1c46478ede7ad065984460cc
LukOls1 <34912980+LukOls1@users.noreply.github.com>
2023-10-01 13:59:07 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerScripts/FirstPersonController/Crosshair.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI interactText;
+    private IInteractable actualInteractable;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && actualInteractable != null)
+        {
+            Debug.Log("used");
+            actualInteractable.Interact();
+            actualInteractable.ShowActionInfo();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {            
+        if (collision.gameObject.tag == "Interactable")
+        {
+            actualInteractable = collision.gameObject.GetComponent<IInteractable>();
+            interactText.text = actualInteractable.ShowActionInfo();
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Interactable" )
+        {
+            Debug.Log("costamten");
+            interactText.text = "";
+            actualInteractable = null;
+            
+        }
+    }
+}
